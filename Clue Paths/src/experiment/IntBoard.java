f2384cdcee40b2bed94a72ef0faee7d487019287
unknown <crice@MZ022-36.adit.mines.edu>
2013-09-26 11:52:09 -0600

    Initial Commit, now with code\!

---
+++ Clue Paths/src/experiment/IntBoard.java
@@ -0,0 +1,29 @@
+package experiment;
+
+import java.util.LinkedList;
+import java.util.Set;
+
+public class IntBoard {
+	public IntBoard(){
+		
+	}
+	
+	public void calcAdjacencies(){
+		
+	}
+	public void startTargets(int row, int col, int numSteps){
+		
+	}
+	
+	public Set<Integer> getTargets(){
+		return new Set<Integer>();
+	}
+	
+	public LinkedList<Integer> getAdjList(){
+		
+	}
+	
+	public int calcIndex(int row, int col){
+		return 0;
+	}
+}
